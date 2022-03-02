a2eec395d64007914dcd537c6f211c8ef08285e2
Tim Schulz <tim@getbej.com>
2022-03-02 13:50:40 +0000

    Fetch GitHub repo information for users

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,5 +1,5 @@
 function userInformationHTML(user) {
-  return `
+    return `
       <h2>${user.name}
           <span class="small-name">
               (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
@@ -13,7 +13,28 @@ function userInformationHTML(user) {
           </div>
           <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
       </div>`;
-}
+};
+
+function repoInformationHTML(repos) {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    let listItemsHTML = repos.map(function(repo) {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`;
+};
 
 function fetchGitHubInformation(event) {
     let username = $("#gh-username").val();
@@ -27,10 +48,15 @@ function fetchGitHubInformation(event) {
     </div>`
     );
 
-    $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
-        function (response) {
-            var userData = response;
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
+    ).then(
+        function (firstResponse, secondResponse) {
+            let userData = firstResponse[0];
+            let repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function (errorResponse) {
             if (errorResponse.status === 404) {
